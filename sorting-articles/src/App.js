62b9a101ca4efd21dfa680dd5fc14603c51208ed
pepe <pepe@DESKTOP-JT5G5K0>
2020-12-07 17:47:33 -0500

    finished sorting-articles exercise

--- sorting-articles/src/App.js
+++ sorting-articles/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -6,17 +6,44 @@ import Articles from './components/Articles';
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+function App({ articles }) {
 
+    const [sortedVotes, setSortedVotes] = useState(articles.sort(upvoteFunc))
+
+    function upvoteFunc(a, b) {
+        return b.upvotes - a.upvotes
+    }
+
+    function sortUpvotes() {
+        articles.sort(upvoteFunc)
+        setSortedVotes(articles)
+    }
+
+    function sortDate() {
+        const aux = [...articles.sort((a, b) => {
+
+            const ad = new Date(a.date)
+            const bd = new Date(b.date)
+            if (ad > bd) {
+                return -1
+            }
+            if (ad < bd) {
+                return 1
+            }
+            return 0
+
+        })]
+        setSortedVotes(aux)
+    }
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button data-testid="most-upvoted-link" className="small" onClick={() => { sortUpvotes() }}>Most Upvoted</button>
+                <button data-testid="most-recent-link" className="small" onClick={() => { sortDate() }}>Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={sortedVotes} />
         </div>
     );
 
--- sorting-articles/src/components/Articles.js
+++ sorting-articles/src/components/Articles.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
-function Articles() {
+function Articles({ articles }) {
 
     return (
         <div className="card w-50 mx-auto">
             <table>
                 <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
+                    <tr>
+                        <th>Title</th>
+                        <th>Upvotes</th>
+                        <th>Date</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
+                    {articles.map((e,i) => {
+                        return (
+                            <tr data-testid="article" key={i}>
+                                <td data-testid="article-title">{e.title}</td>
+                                <td data-testid="article-upvotes">{e.upvotes}</td>
+                                <td data-testid="article-date">{e.date}</td>
+                            </tr>
+
+                        )
+                    })}
                 </tbody>
             </table>
         </div>
